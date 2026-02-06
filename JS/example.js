e81925126e237a281b4fa881c8f6b66a2584a131
chitrapandey8 <chitranshipandey8@gmail.com>
2026-02-07 01:06:47 +0530

    todays

--- JS/example.js
+++ JS/example.js
@@ -918,95 +918,131 @@ const { setTimeout } = require('timers/promises');
 
 // })
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Account {
-    // Protected property convention
-    _accountHolder;
+// class Account {
+//     // Protected property convention
+//     _accountHolder;
     
-    // Private property (Truly private)
-    #balance; 
+//     // Private property (Truly private)
+//     #balance; 
 
-    constructor(name, initialBalance) {
-        this._accountHolder = name;
-        this.#balance = initialBalance;
-        this.transactions = []; // Array to store history
-    }
+//     constructor(name, initialBalance) {
+//         this._accountHolder = name;
+//         this.#balance = initialBalance;
+//         this.transactions = []; // Array to store history
+//     }
 
-    // Getter for balance (Read-only for outside world)
-    get balance() {
-        return this.#balance;
-    }
+//     // Getter for balance (Read-only for outside world)
+//     get balance() {
+//         return this.#balance;
+//     }
 
-    // Protected-style method to modify balance
-    _modifyBalance(amount) {
-        this.#balance += amount;
-        this.transactions.push({
-            type: amount > 0 ? 'CREDIT' : 'DEBIT',
-            amount: amount,
-            date: new Date()
-        });
-    }
+//     // Protected-style method to modify balance
+//     _modifyBalance(amount) {
+//         this.#balance += amount;
+//         this.transactions.push({
+//             type: amount > 0 ? 'CREDIT' : 'DEBIT',
+//             amount: amount,
+//             date: new Date()
+//         });
+//     }
 
 
-    processsInternatioaltransaction(amount,callback){
-    return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-     if(amount>10000){
-     reject("audit required")
-    }else{
-      resolve(callback)
-    }
-    },2000)
+//     processsInternatioaltransaction(amount,callback){
+//     return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//      if(amount>10000){
+//      reject("audit required")
+//     }else{
+//       resolve(callback)
+//     }
+//     },2000)
     
-   })
-}
+//    })
+// }
 
-    displaySummary() {
-        console.log(`Holder: ${this._accountHolder} | Balance: $${this.balance}`);
-    }
-}
+//     displaySummary() {
+//         console.log(`Holder: ${this._accountHolder} | Balance: $${this.balance}`);
+//     }
+// }
 
 
-class InvestmentAccount extends Account{
-    static INTEREST_RATE = 0.05
-     #lockedfunds  = true
+// class InvestmentAccount extends Account{
+//     static INTEREST_RATE = 0.05
+//      #lockedfunds  = true
      
 
-     set locked(status){
-      this.#lockedfunds = status
-     }
+//      set locked(status){
+//       this.#lockedfunds = status
+//      }
 
-     _modifyBalance(amount){
-       if(this.#lockedfunds == true && amount<0){
-        throw new Error("chl be bareeb")
-       } 
-     }
-}
+//      _modifyBalance(amount){
+//        if(this.#lockedfunds == true && amount<0){
+//         throw new Error("chl be bareeb")
+//        } 
+//      }
+// }
 
-const accountobj = new Account("chitranshi",50000)
-accountobj._modifyBalance(10000)
+// const accountobj = new Account("chitranshi",50000)
+// accountobj._modifyBalance(10000)
 
 
-const fs = require('fs').promises
-async function generateMonthlystatemnt(accountobj){
-  try{
-   const {_accountHolder,transactions} = accountobj
-   const filename = `statement_${_accountHolder}.txt`
-   const  data = JSON.stringify(transactions)
-   await fs.writeFile(filename,data)
-  }catch(error){
-   throw  new Error("file de")
-  }
+// const fs = require('fs').promises
+// async function generateMonthlystatemnt(accountobj){
+//   try{
+//    const {_accountHolder,transactions} = accountobj
+//    const filename = `statement_${_accountHolder}.txt`
+//    const  data = JSON.stringify(transactions)
+//    await fs.writeFile(filename,data)
+//   }catch(error){
+//    throw  new Error("file de")
+//   }
   
    
+// }
+// generateMonthlystatemnt(accountobj)
+
+
+
+// accountobj.processsInternatioaltransaction(1010101,()=>{console.log("transfer completed");
+// }).then((data)=>{
+//   data()
+// }).catch((err)=>{
+// console.log(err);
+
+// })
+
+// function settimeoutt(time,callback){
+//     let starttime = Date.now()
+//     while(true){
+//       let currenttime = Date.now()
+//       if(currenttime-starttime>=time){
+//         callback()
+//         return
+//       }
+//     }
+// }
+
+// function hello(){
+//   console.log("hello");
+  
+// }
+// settimeoutt(2000,hello)
+
+function math(){
+
 }
-generateMonthlystatemnt(accountobj)
+
+math.prototype.sum = function(...nums){
+    let ans = nums.reduce((sum,defaul)=>{
+       return sum + defaul
+     },0)
+
+     return ans;
+}
+
+let obj = new math()
+let ans1 = obj.sum(1,2,3,4,5)
+console.log(ans1);
 
 
 
-accountobj.processsInternatioaltransaction(1010101,()=>{console.log("transfer completed");
-}).then((data)=>{
-  data()
-}).catch((err)=>{
-console.log(err);
-
-})
