cc4525139a677a96ed9020da6721f9ab8a046b1b
chitrapandey8 <chitranshipandey8@gmail.com>
2026-03-06 01:38:03 +0530

    zustand

--- React/react/src/App.tsx
+++ React/react/src/App.tsx
@@ -1,21 +1,40 @@
-import { useEffect, useState, type ChangeEvent } from "react"
-import { useDebounce } from "./Hooks/UseDebounce/UseDebouncee"
+// import { useEffect, useState, type ChangeEvent } from "react"
+// import { useDebounce } from "./Hooks/UseDebounce/UseDebouncee"
+
+import { useContext } from "react"
+import { useCounter } from "./store"
+
+// const App = () => {
+//   const [inputValue,setInputValue] = useState<string>("")
+//   const debounceValue =  useDebounce(inputValue,1)
+
+//   function onChangeHandler(e:ChangeEvent<HTMLInputElement>){
+//     setInputValue(e.target.value)
+//   }
+
+//   useEffect(()=>{
+//     console.log("Backend Called");
+//   },[debounceValue])
+
+//   return (
+//     <div>
+//       <input type="text" placeholder="Enter Whatever" onChange={onChangeHandler}  />    
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 
 const App = () => {
-  const [inputValue,setInputValue] = useState<string>("")
-  const debounceValue =  useDebounce(inputValue,1)
-
-  function onChangeHandler(e:ChangeEvent<HTMLInputElement>){
-    setInputValue(e.target.value)
-  }
-
-  useEffect(()=>{
-    console.log("Backend Called");
-  },[debounceValue])
+   const {count, increment, decrement} = useCounter()
 
   return (
     <div>
-      <input type="text" placeholder="Enter Whatever" onChange={onChangeHandler}  />    
+      {count}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   )
 }
